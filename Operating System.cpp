16c4617f26a0a103e7eefa04334d35ca1c7028ad
Miguel Miranda <miguelmiranda.tech@gmail.com>
2025-02-04 16:28:21 -0600

    Created void bootingSystem

--- Operating System.cpp
+++ Operating System.cpp
@@ -1,20 +1,28 @@
-// Operating System.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+void bootingSystem(std::string username) {
+    std::cout << "Hello " << username << "!\n";
+    std::cout << "Updating and verifying services...\n";
+    std::cout << "10\n";
+    std::cout << "9\n";
+    std::cout << "8\n";
+    std::cout << "7\n";
+    std::cout << "6\n";
+    std::cout << "5\n";
+    std::cout << "4\n";
+    std::cout << "3\n";
+    std::cout << "2\n";
+    std::cout << "1\n";
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string username;
+    std::cout << "Username: ";
+    std::cin >> username;
+    bootingSystem(username);
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
